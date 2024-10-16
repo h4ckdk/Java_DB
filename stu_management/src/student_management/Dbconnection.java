89e1d2ede4af19a5e321447e4cff56e0fe0cdc82
h4ckdk <hackdk004@proton.me>
2024-10-16 21:21:53 +0530

    uploaded

---
+++ stu_management/src/student_management/Dbconnection.java
@@ -0,0 +1,15 @@
+package student_management;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Dbconnection {
+    private static final String url = "jdbc:mysql://localhost:3306/details";
+    private static final String user = "root";
+    private static final String password = "password";
+
+    public static Connection getconnection() throws SQLException {
+        return DriverManager.getConnection(url,user,password);
+    }
+}
